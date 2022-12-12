7b211ccbae4ed95b683f6a7aef2555bd407916f2
Rommel Bondoc <rpbondocbiz@gmail.com>
2022-12-12 18:33:01 +0800

    BMI New Version

--- automated-testing/bmi_calculator.test.js
+++
@@ -1,5 +0,0 @@
-const bmi_calculator = require("./bmi_calculator");
-
-test("Checks if BMI < 18.5, the prompt displays You are underweight!", () => {
-    expect(BMIConclusion.bmi_calculator).toBe("You are underweight!");
-});
